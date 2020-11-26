c51a000e96c7f8f07663a2042b0fd1926ae50a89
Plyusnin Artyom <a.plyusnin@g.nsu.ru>
2020-11-26 11:45:04 +0700

    Add lazy reading of types + tests

--- TowerDefense/src/main/java/ru/nsu/fit/towerdefense/model/world/types/ProjectileType.java
+++ TowerDefense/src/main/java/ru/nsu/fit/towerdefense/model/world/types/ProjectileType.java
@@ -1,37 +1,124 @@
 package ru.nsu.fit.towerdefense.model.world.types;
 
+import java.util.HashMap;
 import java.util.Map;
 import ru.nsu.fit.towerdefense.model.world.Vector2;
 
 public class ProjectileType {
-  private float speed;
-  private boolean selfGuided;
-  private Map<EnemyType, Integer> enemyTypeDamageMap;
-  private int hitBox;
-  private Vector2<Double> size;
-  private String image;
+    private String typeName;
+    private float speed;
+    private boolean selfGuided;
+    private int basicDamage;
+    private Map<String, Integer> enemyTypeDamageMap;
+    private int hitBox;
+    private Vector2<Double> size;
+    private String image;
 
-  public Vector2<Double> getSize() {
+    public Vector2<Double> getSize() {
     return size;
   }
 
-  public String getImage() {
+    public String getImage() {
     return image;
   }
 
-  public float getSpeed() {
+    public float getSpeed() {
     return speed;
   }
 
-  public boolean isSelfGuided() {
+    public boolean isSelfGuided() {
     return selfGuided;
   }
 
-  public Map<EnemyType, Integer> getEnemyTypeDamageMap() {
+    public Map<String, Integer> getEnemyTypeDamageMap() {
     return enemyTypeDamageMap;
   }
 
-  public int getHitBox() {
+    public int getHitBox() {
     return hitBox;
   }
+
+    public int getBasicDamage()
+    {
+        return basicDamage;
+    }
+
+    public String getTypeName()
+    {
+        return typeName;
+    }
+
+    public static class Builder
+    {
+        private String typeName;
+        private float speed;
+        private boolean selfGuided;
+        private Map<String, Integer> enemyTypeDamageMap;
+        private int hitBox;
+        private Vector2<Double> size;
+        private int basicDamage;
+        private String image;
+        public Builder(String name)
+        {
+            typeName = name;
+            enemyTypeDamageMap = new HashMap<>();
+            size = new Vector2<>(0.0, 0.0);
+        }
+
+        public void setImage(String image)
+        {
+            this.image = image;
+        }
+
+        public void setSpeed(float speed)
+        {
+            this.speed = speed;
+        }
+
+        public void setHitBox(int hitBox)
+        {
+            this.hitBox = hitBox;
+        }
+
+        public void setSize(Double x, Double y)
+        {
+            this.size.setX(x);
+            this.size.setY(y);
+        }
+
+        public void setBasicDamage(int basicDamage)
+        {
+            this.basicDamage = basicDamage;
+        }
+
+        public void setSelfGuided(boolean selfGuided)
+        {
+            this.selfGuided = selfGuided;
+        }
+
+        public void setTypeName(String typeName)
+        {
+            this.typeName = typeName;
+        }
+
+        public void add(String type, Integer basicDamage)
+        {
+            enemyTypeDamageMap.put(type, basicDamage);
+        }
+
+        public ProjectileType build()
+        {
+            ProjectileType type = new ProjectileType();
+            type.typeName = typeName;
+            type.speed = speed;
+            type.size = size;
+            type.selfGuided = selfGuided;
+            type.image = image;
+            type.hitBox = hitBox;
+            type.enemyTypeDamageMap = enemyTypeDamageMap;
+            type.basicDamage = basicDamage;
+            return type;
+        }
+    }
+
 }
--- TowerDefense/src/main/java/ru/nsu/fit/towerdefense/model/world/types/TowerType.java
+++ TowerDefense/src/main/java/ru/nsu/fit/towerdefense/model/world/types/TowerType.java
@@ -1,45 +1,146 @@
 package ru.nsu.fit.towerdefense.model.world.types;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class TowerType {
-  private int price;
-  private List<TowerType> upgrades;
-  private int range;
-  private int fireRate;
-  private FireType fireType;
-  private ProjectileType projectileType;
-  private String image;
 
-  public String getImage() {
+    public static class Upgrade
+    {
+        private String name;
+        private int cost;
+        public Upgrade(String name, int cost)
+        {
+            this.name = name;
+            this.cost = cost;
+        }
+
+        public int getCost() {
+            return cost;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    private String typeName;
+    private int price;
+    private List<Upgrade> upgrades;
+    private int range;
+    private int fireRate;
+    private FireType fireType;
+    private String projectileType;
+    private String image;
+
+    public String getTypeName()
+    {
+        return typeName;
+    }
+
+    public String getImage() {
     return image;
   }
 
-  public int getPrice() {
+    public int getPrice() {
     return price;
   }
 
-  public List<TowerType> getUpgrades() {
+    public List<Upgrade> getUpgrades() {
     return upgrades;
   }
 
-  public int getRange() {
+    public int getRange() {
     return range;
   }
 
-  public int getFireRate() {
+    public int getFireRate() {
     return fireRate;
   }
 
-  public FireType getFireType() {
+    public FireType getFireType() {
     return fireType;
   }
 
-  public ProjectileType getProjectileType() {
+    public String getProjectileType() {
     return projectileType;
   }
 
-  public enum FireType {
-    UNIDIRECTIONAL, OMNIDIRECTIONAL
-  }
+    public enum FireType {
+        UNIDIRECTIONAL, OMNIDIRECTIONAL
+    }
+
+    public static class Builder
+    {
+        private int price;
+        private String typeName;
+        private List<Upgrade> upgrades;
+        private int range;
+        private int fireRate;
+        private String fireType;
+        private String projectileType;
+        private String image;
+
+        public Builder(String typeName)
+        {
+            this.typeName = typeName;
+            upgrades = new LinkedList<>();
+        }
+
+        public void setImage(String image)
+        {
+            this.image = image;
+        }
+
+        public void setFireRate(int fireRate)
+        {
+            this.fireRate = fireRate;
+        }
+
+        public void setFireType(String fireType)
+        {
+            this.fireType = fireType;
+        }
+
+        public void setPrice(int price)
+        {
+            this.price = price;
+        }
+
+        public void setProjectileType(String projectileType)
+        {
+            this.projectileType = projectileType;
+        }
+
+        public void setRange(int range)
+        {
+            this.range = range;
+        }
+
+        public void addUpgrade(Upgrade upgrade)
+        {
+            upgrades.add(upgrade);
+        }
+
+        public TowerType build()
+        {
+            TowerType type = new TowerType();
+            type.typeName = typeName;
+            type.fireRate = fireRate;
+            if (fireType.compareTo("Unidirectional") == 0)
+            {
+                type.fireType = FireType.UNIDIRECTIONAL;
+            }
+            else
+            {
+                type.fireType = FireType.OMNIDIRECTIONAL;
+            }
+            type.image = image;
+            type.price = price;
+            type.projectileType = projectileType;
+            type.range = range;
+            type.upgrades = upgrades;
+            return type;
+        }
+    }
 }
