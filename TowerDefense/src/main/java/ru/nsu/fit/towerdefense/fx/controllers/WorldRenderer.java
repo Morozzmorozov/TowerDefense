2c21c88a181e7153ffbfc0a829559f7599c3b899
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-11-28 01:26:19 +0700

    Delete methods for rendering with memorizing

--- TowerDefense/src/main/java/ru/nsu/fit/towerdefense/fx/controllers/WorldRenderer.java
+++ TowerDefense/src/main/java/ru/nsu/fit/towerdefense/fx/controllers/WorldRenderer.java
@@ -6,9 +6,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import ru.nsu.fit.towerdefense.model.world.gameobject.Renderable;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * WorldRenderer class is used for rendering game objects on the game scene.
@@ -20,7 +17,6 @@ public class WorldRenderer {
     private static final double PIXELS_PER_GAME_CELL = 10;
 
     private final ObservableList<Node> gameNodes;
-    private final Map<Renderable, Node> renderableToGameNodeMap = new HashMap<>();
 
     /**
      * Creates new WorldRenderer with specified game nodes.
@@ -29,51 +25,6 @@ public class WorldRenderer {
      */
     public WorldRenderer(ObservableList<Node> gameNodes) {
         this.gameNodes = gameNodes;
-    }
-
-    /**
-     * Updates a map (Renderable -> game node) with new renderables.
-     *
-     * @param newRenderableSet updated renderables.
-     */
-    public void update(Set<Renderable> newRenderableSet) {
-        for (Renderable renderable : newRenderableSet) {
-            if (!renderableToGameNodeMap.containsKey(renderable)) {
-                Rectangle rectangle = new Rectangle(
-                    renderable.getSize().getX() * PIXELS_PER_GAME_CELL,
-                    renderable.getSize().getY() * PIXELS_PER_GAME_CELL,
-                    Color.DODGERBLUE);
-                rectangle.setUserData(renderable);
-                renderableToGameNodeMap.put(renderable, rectangle);
-            }
-        }
-
-        renderableToGameNodeMap.entrySet().removeIf(entry -> !newRenderableSet.contains(entry.getKey()));
-    }
-
-    /**
-     * Renders previously updated renderables.
-     *
-     * Must be called in JavaFX Application thread!
-     */
-    public void render() {
-        gameNodes.removeIf(node -> !renderableToGameNodeMap.containsKey(node.getUserData()));
-
-        for (Map.Entry<Renderable, Node> entry : renderableToGameNodeMap.entrySet()) {
-            Renderable renderable = entry.getKey();
-            Rectangle rectangle = (Rectangle) entry.getValue();
-
-            rectangle.relocate(
-                renderable.getPosition().getX() * PIXELS_PER_GAME_CELL,
-                renderable.getPosition().getY() * PIXELS_PER_GAME_CELL);
-
-            rectangle.setWidth(renderable.getSize().getX() * PIXELS_PER_GAME_CELL);
-            rectangle.setHeight(renderable.getSize().getY() * PIXELS_PER_GAME_CELL);
-
-            if (!gameNodes.contains(rectangle)) {
-                gameNodes.add(rectangle);
-            }
-        }
     }
 
     /**
