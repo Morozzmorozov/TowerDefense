34fddb432263be8bf9ff3dee303476c042799a25
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-12-03 22:43:32 +0700

    Add rendering with memorizing

--- TowerDefense/src/main/java/ru/nsu/fit/towerdefense/fx/controllers/WorldRenderer.java
+++ TowerDefense/src/main/java/ru/nsu/fit/towerdefense/fx/controllers/WorldRenderer.java
@@ -7,7 +7,10 @@ import ru.nsu.fit.towerdefense.fx.Images;
 import ru.nsu.fit.towerdefense.fx.exceptions.RenderException;
 import ru.nsu.fit.towerdefense.model.world.gameobject.Renderable;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 /**
  * WorldRenderer class is used for rendering game objects on the game scene.
@@ -18,6 +21,8 @@ public class WorldRenderer {
 
     private final ObservableList<Node> gameNodes;
     private final double pixelsPerGameCell;
+
+    private final Map<Renderable, Node> renderableToGameNodeMap = new HashMap<>();
 
     /**
      * Creates new WorldRenderer with specified game nodes.
@@ -55,6 +60,59 @@ public class WorldRenderer {
                 gameNodes.add(imageView);
             } catch (NoSuchElementException e) {
                 throw new RenderException("No image was found by name \"" + renderable.getImageName() + "\".");
+            }
+        }
+    }
+
+    /**
+     * Updates a map (Renderable -> game node) with new renderables.
+     *
+     * @param newRenderableSet updated renderables.
+     */
+    public void update(Set<Renderable> newRenderableSet) throws RenderException {
+        for (Renderable renderable : newRenderableSet) {
+            if (!renderableToGameNodeMap.containsKey(renderable)) {
+                try {
+                    ImageView imageView =
+                        new ImageView(Images.getInstance().getImage(renderable.getImageName()));
+
+                    imageView.setOnMouseClicked(mouseEvent -> System.out.println(renderable.getImageName()));
+
+                    imageView.setUserData(renderable);
+                    renderableToGameNodeMap.put(renderable, imageView);
+                } catch (NoSuchElementException e) {
+                    throw new RenderException("No image was found by name \"" + renderable.getImageName() + "\".");
+                }
+            }
+        }
+
+        renderableToGameNodeMap.entrySet().removeIf(entry -> !newRenderableSet.contains(entry.getKey()));
+    }
+
+    /**
+     * Renders previously updated renderables.
+     *
+     * Must be called in JavaFX Application thread!
+     */
+    public void render() {
+        gameNodes.removeIf(node -> !renderableToGameNodeMap.containsKey(node.getUserData()));
+
+        for (Map.Entry<Renderable, Node> entry : renderableToGameNodeMap.entrySet()) {
+            Renderable renderable = entry.getKey();
+            ImageView imageView = (ImageView) entry.getValue();
+
+            imageView.relocate(
+                renderable.getPosition().getX() * pixelsPerGameCell,
+                renderable.getPosition().getY() * pixelsPerGameCell);
+
+            imageView.setFitWidth(renderable.getSize().getX() * pixelsPerGameCell);
+            imageView.setFitHeight(renderable.getSize().getY() * pixelsPerGameCell);
+
+            imageView.setRotate(renderable.getRotation());
+            imageView.setViewOrder(-renderable.getZIndex());
+
+            if (!gameNodes.contains(imageView)) {
+                gameNodes.add(imageView);
             }
         }
     }
