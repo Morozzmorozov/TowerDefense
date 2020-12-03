f2ac19f80e1accf0417965c5a9e8de6aae19cac1
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-12-03 22:42:01 +0700

    Add getZIndex() in Renderable

--- TowerDefense/src/main/java/ru/nsu/fit/towerdefense/model/world/gameobject/Renderable.java
+++ TowerDefense/src/main/java/ru/nsu/fit/towerdefense/model/world/gameobject/Renderable.java
@@ -7,7 +7,21 @@ public interface Renderable {
   Vector2<Double> getSize();
   String getImageName();
 
+  /**
+   * Returns rotation in degrees.
+   *
+   * @return rotation in degrees.
+   */
   default double getRotation() {
+    return 0;
+  }
+
+  /**
+   * Returns z-index (it must be >= 0). The bigger z-index the later this object will be rendered.
+   *
+   * @return z-index.
+   */
+  default double getZIndex() {
     return 0;
   }
 }
