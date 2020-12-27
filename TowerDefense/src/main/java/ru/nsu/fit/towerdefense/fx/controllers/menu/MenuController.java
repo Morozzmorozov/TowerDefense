9b3f05ece265dabbc17764c1bc043ce92faa32a8
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-12-27 13:46:52 +0700

    Add stub buttons for replays to MenuController

--- TowerDefense/src/main/java/ru/nsu/fit/towerdefense/fx/controllers/menu/MenuController.java
+++ TowerDefense/src/main/java/ru/nsu/fit/towerdefense/fx/controllers/menu/MenuController.java
@@ -1,13 +1,21 @@
 package ru.nsu.fit.towerdefense.fx.controllers.menu;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import ru.nsu.fit.towerdefense.fx.SceneManager;
 import ru.nsu.fit.towerdefense.fx.controllers.Controller;
+import ru.nsu.fit.towerdefense.fx.util.AlertBuilder;
 import ru.nsu.fit.towerdefense.metadata.GameMetaData;
+import ru.nsu.fit.towerdefense.replay.GameStateReader;
+import ru.nsu.fit.towerdefense.replay.Replay;
 
 /**
  * MenuController class is used by JavaFX in javafx.fxml.FXMLLoader for showing a menu scene.
@@ -41,11 +49,31 @@ public class MenuController implements Controller {
         }
 
         for (String gameMapName : GameMetaData.getInstance().getGameMapNames()) {
-            Button button = new Button(gameMapName);
+            Button gameButton = new Button(gameMapName);
+            gameButton.setOnAction(actionEvent -> sceneManager.switchToGame(gameMapName));
 
-            button.setOnAction(actionEvent -> sceneManager.switchToGame(gameMapName));
+            ComboBox<String> comboBox = new ComboBox<>(FXCollections.observableArrayList(
+                GameStateReader.getInstance().getReplays(gameMapName)));
+            comboBox.getSelectionModel().selectFirst();
 
-            buttonsVBox.getChildren().add(button);
+            Button replayButton = new Button("Replay");
+            replayButton.setOnAction(actionEvent -> {
+                Replay replay = GameStateReader.getInstance().readReplay(gameMapName, comboBox.getValue());
+                if (replay != null) {
+                    sceneManager.switchToGame(gameMapName, replay);
+                } else {
+                    new AlertBuilder()
+                        .setHeaderText("Replay is null!")
+                        .setOwner(sceneManager.getWindowOwner())
+                        .build().showAndWait();
+                }
+            });
+
+            buttonsVBox.getChildren().add(new Group() {{
+                getChildren().add(new HBox() {{
+                    getChildren().addAll(gameButton, replayButton, comboBox);
+                }});
+            }});
         }
     }
 
