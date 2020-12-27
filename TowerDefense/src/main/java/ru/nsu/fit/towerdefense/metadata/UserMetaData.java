1e10020b02854462f5f9e4e0148a4937605d452d
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-12-27 13:45:16 +0700

    Create UserMetaData for saving ResearchPoints & UnlockedResearchNames

---
+++ TowerDefense/src/main/java/ru/nsu/fit/towerdefense/metadata/UserMetaData.java
@@ -0,0 +1,40 @@
+package ru.nsu.fit.towerdefense.metadata;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.prefs.Preferences;
+
+public class UserMetaData {
+
+    private static final String RESEARCH_POINTS_KEY = "researchPoints";
+
+    private static final Preferences PREFERENCES = Preferences.userRoot().node(UserMetaData.class.getName());
+
+    public static int getResearchPoints() {
+        return PREFERENCES.getInt(RESEARCH_POINTS_KEY, 0);
+    }
+
+    public static void setResearchPoints(int researchPoints) {
+        PREFERENCES.putInt(RESEARCH_POINTS_KEY, researchPoints);
+    }
+
+    public static void addResearchPoints(int researchPoints) {
+        PREFERENCES.putInt(RESEARCH_POINTS_KEY, getResearchPoints() + researchPoints);
+    }
+
+    public static List<String> getUnlockedResearchNames(List<String> availableResearchNames) {
+        List<String> unlockedResearchNames = new ArrayList<>();
+
+        for (String availableResearchName : availableResearchNames) {
+            if (PREFERENCES.getBoolean(availableResearchName, false)) {
+                unlockedResearchNames.add(availableResearchName);
+            }
+        }
+
+        return unlockedResearchNames;
+    }
+
+    public static void saveResearch(String researchName) {
+        PREFERENCES.putBoolean(researchName, true);
+    }
+}
