70636f1384358e3eb4d33e561ad274b125a6ae37
Plyusnin Artyom <a.plyusnin@g.nsu.ru>
2020-12-28 11:36:55 +0700

    add list of tech to draw

--- TowerDefense/src/main/java/ru/nsu/fit/towerdefense/metadata/techtree/TechTree.java
+++ TowerDefense/src/main/java/ru/nsu/fit/towerdefense/metadata/techtree/TechTree.java
@@ -1,7 +1,7 @@
 package ru.nsu.fit.towerdefense.metadata.techtree;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import javax.print.attribute.standard.RequestingUserName;
+import java.util.*;
 
 public class TechTree {
 	private ArrayList<Research> research;
@@ -72,5 +72,60 @@ public class TechTree {
 	{
 		this.unlocked = unlocked;
 	}
+
+	public List<List<Research>> getSortedResearches() throws NoSuchElementException
+	{
+		Queue<Research> queue = new LinkedList<>();
+		HashMap<Research, Integer> nameToLevel = new HashMap<>();
+		HashMap<Research, Integer> nameToLeft = new HashMap<>();
+		int rest = 0;
+ 		for (var x : research)
+	    {
+	    	rest++;
+	    	nameToLeft.put(x, x.getLeft());
+	    	nameToLevel.put(x, 0);
+	    }
+		for (var x : availableResearches)
+		{
+			queue.add(x);
+			nameToLevel.put(x, 0);
+		}
+
+		List<List<Research>> result = new ArrayList<>();
+
+
+ 		while (queue.size() > 0)
+	    {
+	    	Research r = queue.poll();
+	    	int cur = nameToLevel.get(r);
+	    	if (result.size() <= cur)
+		    {
+		    	List<Research> list = new LinkedList<>();
+		    	result.add(list);
+		    }
+	    	result.get(cur).add(r);
+
+	    	rest--;
+
+	    	for (var to : r.getInfluence())
+		    {
+		    	int t = nameToLeft.get(to);
+		    	int level = nameToLevel.get(to);
+		    	level = Math.max(level, cur + 1);
+		    	nameToLeft.put(to, t - 1);
+			    nameToLevel.put(to, level);
+		    	if (t == 1)
+			    {
+			    	queue.add(to);
+			    }
+		    }
+	    }
+ 		if (rest > 0)
+	    {
+	    	throw new NoSuchElementException("Tree has cycle");
+	    }
+ 		return result;
+	}
+
 
 }
