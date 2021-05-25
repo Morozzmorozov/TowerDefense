a684df2b502a2be52133da02f80146fc5e2e9a35
omarkelov <36790281+omarkelov@users.noreply.github.com>
2021-05-25 18:24:50 +0700

    Add global mappings

---
+++ TowerDefense/src/main/java/ru/nsu/fit/towerdefense/server/Mappings.java
@@ -0,0 +1,10 @@
+package ru.nsu.fit.towerdefense.server;
+
+public class Mappings {
+    public static final String LOGIN_MAPPING = "/login";
+    public static final String LOBBY_MAPPING = "/lobby/*";
+    public static final String JOIN_LOBBY_MAPPING = "/lobby/join";
+    public static final String LEAVE_LOBBY_MAPPING = "/lobby/leave";
+    public static final String LOBBIES_MAPPING = "/lobbies";
+    public static final String CREATE_LOBBY_MAPPING = "/createlobby";
+}
