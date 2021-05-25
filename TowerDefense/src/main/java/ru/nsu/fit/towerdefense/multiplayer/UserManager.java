682c0be1e8c4fd202ab142f13c4c3118f297edcf
omarkelov <36790281+omarkelov@users.noreply.github.com>
2021-05-25 17:50:06 +0700

    Impl lobby creating

--- TowerDefense/src/main/java/ru/nsu/fit/towerdefense/multiplayer/UserManager.java
+++ TowerDefense/src/main/java/ru/nsu/fit/towerdefense/multiplayer/UserManager.java
@@ -1,5 +1,10 @@
 package ru.nsu.fit.towerdefense.multiplayer;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import ru.nsu.fit.towerdefense.multiplayer.entities.Lobby;
+import ru.nsu.fit.towerdefense.multiplayer.entities.Session;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -15,11 +20,8 @@ public class UserManager {
     private static final String SITE_URI = "http://127.0.0.1:8080";
 
     private static final String LOGIN_MAPPING = "/login";
-    private static final String LOGOUT_MAPPING = "/logout";
-    private static final String PING_MAPPING = "/ping";
-    private static final String RAW_MOVIE = "/raw-movie";
-    private static final String RAW_MOVIES = "/raw-movies";
-    private static final String RAW_PHRASE = "/raw-phrase";
+    private static final String CREATE_LOBBY_MAPPING = "/createlobby";
+    private static final String JOIN_LOBBY_MAPPING = "/lobby/join";
 
     private Credentials credentials = new Credentials();
 
@@ -57,5 +59,63 @@ public class UserManager {
 
     public void logout() {
         credentials = new Credentials();
+    }
+
+    public String createLobby(String gameMapName) {
+        if (true)
+            return "id_123"; // todo delete
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(SITE_URI + CREATE_LOBBY_MAPPING +
+                    "?username=" + credentials.getUsername() +
+                    "&password=" + credentials.getPassword() +
+                    "&map=" + gameMapName))
+                .timeout(Duration.of(15, SECONDS))
+                .build();
+
+            HttpResponse<String> response = HttpClient.newBuilder()
+                .build()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() >= 200 && response.statusCode() < 300) {
+                return new Gson().fromJson(response.body(), Lobby.class).getId();
+            }
+
+            System.out.println("Bad status code: " + response.statusCode());
+            return null;
+        } catch (URISyntaxException | IOException | InterruptedException | JsonSyntaxException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String joinLobby(String lobbyId) {
+        if (true)
+            return "id_123"; // todo delete
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(SITE_URI + JOIN_LOBBY_MAPPING +
+                    "?username=" + credentials.getUsername() +
+                    "&password=" + credentials.getPassword() +
+                    "&lobbyId=" + lobbyId))
+                .timeout(Duration.of(15, SECONDS))
+                .build();
+
+            HttpResponse<String> response = HttpClient.newBuilder()
+                .build()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() >= 200 && response.statusCode() < 300) {
+                return new Gson().fromJson(response.body(), Session.class).getToken();
+            }
+
+            System.out.println("Bad status code: " + response.statusCode());
+            return null;
+        } catch (URISyntaxException | IOException | InterruptedException | JsonSyntaxException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
---
+++ TowerDefense/src/main/java/ru/nsu/fit/towerdefense/multiplayer/entities/Lobby.java
@@ -0,0 +1,13 @@
+package ru.nsu.fit.towerdefense.multiplayer.entities;
+
+public class Lobby {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
